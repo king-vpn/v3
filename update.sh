d73ab2d4fb5a6fbd36c5cb0af87c791f8856917e
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-02-17 21:25:55 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/AngIMAN/v3/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/king-vpn/v3/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
